256df2c6df5be23ee3d4baed82867883acd6eaf3
Maksim Vilcheuski <maxmaxwow@gmail.com>
2017-11-08 14:54:43 +0300

    Added uploading and getting image

--- src/main/java/com/ecommerceserver/controllers/ImageController.java
+++ src/main/java/com/ecommerceserver/controllers/ImageController.java
@@ -1,12 +1,14 @@
 package com.ecommerceserver.controllers;
 
 import com.ecommerceserver.service.ImageServiceImpl;
+import com.ecommerceserver.service.dto.ImageDto;
+import com.ecommerceserver.service.exception.BadImageSizeException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/image")
@@ -15,10 +17,21 @@ public class ImageController {
     @Resource(name = "imageService")
     private ImageServiceImpl imageService;
 
-    @GetMapping(value = "/get")
-    public ResponseEntity getImage()
+    @GetMapping(value = "/{imageName:.+}", produces = "image/jpeg")
+    public ResponseEntity<org.springframework.core.io.Resource> getImage(@PathVariable String imageName)
     {
+        org.springframework.core.io.Resource file = this.imageService.getFullImageAsResource(imageName);
+        return ResponseEntity.ok(file);
+    }
 
-        return ResponseEntity.ok(null);
+    @PostMapping(value = "/image", produces = "application/json")
+    public ResponseEntity<ImageDto> uploadImage(@RequestParam("image") MultipartFile image) throws IOException, BadImageSizeException, InterruptedException
+    {
+        if (image == null || !imageService.isImage(image))
+        {
+            return ResponseEntity.badRequest().build();
+        }
+        ImageDto result = imageService.saveImage(image);
+        return ResponseEntity.ok(result);
     }
 }
