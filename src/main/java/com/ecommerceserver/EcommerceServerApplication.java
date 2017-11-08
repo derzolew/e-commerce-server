cf24113282a32a0fb7a567dc32b03b40ed70f10c
Maksim Vilcheuski <maxmaxwow@gmail.com>
2017-11-08 11:08:21 +0300

    init project

---
+++ src/main/java/com/ecommerceserver/EcommerceServerApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceServerApplication.class, args);
+	}
+}
