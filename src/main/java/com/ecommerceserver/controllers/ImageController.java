0fbc18c18955a03ca31cc64ff90632c10a3cb1da
Maksim Vilcheuski <maxmaxwow@gmail.com>
2017-11-08 11:52:59 +0300

    Some changes

--- src/main/java/com/ecommerceserver/controllers/ImageController.java
+++ src/main/java/com/ecommerceserver/controllers/ImageController.java
@@ -1,4 +1,18 @@
 package com.ecommerceserver.controllers;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/image")
 public class ImageController {
+
+    @GetMapping(value = "/get")
+    public ResponseEntity<Resource> getImage()
+    {
+        return ResponseEntity.ok(null);
+    }
 }
---
+++ src/main/java/com/ecommerceserver/repository/ImageRepository.java
@@ -0,0 +1,4 @@
+package com.ecommerceserver.repository;
+
+/*public class ImageRepository implements JpaRepository<<?>, Long> {
+}*/
